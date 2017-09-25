c060094585d665231277ae0a927b2b026e6ba903
Greg Cusack <gregorycusack@gmail.com>
2017-09-25 12:36:28 -0600

    edit tor_list and scrape_tor_list.py

--- scrape_tor_list.py
+++ scrape_tor_list.py
@@ -5,6 +5,6 @@ import csv
 url = "https://torstatus.blutmagie.de/ip_list_all.php/Tor_ip_list_ALL.csv"
 response = urllib2.urlopen(url)
 cr = csv.reader(response)
-writer = csv.writer(open('/Users/gregcusack/Desktop/ECEN 5003 (Wustrow)/Tor Presentation/tor_ips.txt', 'w'))
+writer = csv.writer(open('/home/greg/Desktop/ECEN_5003_Wustrow/tor_classifier/tor_ips.txt', 'w'))
 for row in cr:
     writer.writerow(row)
